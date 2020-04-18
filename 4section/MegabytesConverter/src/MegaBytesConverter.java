2fe172ab72a5248409618f812530812b737fe436
crispane <crispane@pm.me>
2020-04-18 12:23:54 +0300

    Exercise MebabytesConverter

---
+++ 4section/MegabytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,11 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kilobytes) {
+        if (kilobytes < 0)
+            System.out.println("Invalid Value");
+        else {
+            int megabytes = kilobytes / 1024;
+            int kb = kilobytes % 1024;
+            System.out.println(kilobytes + " KB = " + megabytes + " MB " + "and " + kb + " KB");
+        }
+    }
+}
